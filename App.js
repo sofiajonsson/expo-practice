f0661d68837d290e67886b84fda443a8303f553b
Sofia <jonssonsofia93@gmail.com>
2019-11-01 13:13:38 -0700

    Added toggle feature through flexbox

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react'
-import { Image, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+
+import Tog from './Tog'
 
 export default class App extends Component {
+
+  state = {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
   render() {
+    const {flexDirection, alignItems, justifyContent} = this.state
+    const layoutStyle = {flexDirection, justifyContent, alignItems}
+
+    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
+    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
+
     return (
-      <Image
-        style={styles.image}
-        source={{uri: 'https://www.jonssonsofia.com/static/media/sofia-milo.fe008a5f.jpeg'}}
-      />
+      <View style={styles.container}>
+        <Tog
+          label={'Primary axis (flexDirection)'}
+          value={flexDirection}
+          options={['row', 'column']}
+          onChange={(option) => this.setState({flexDirection: option})}
+        />
+        <Tog
+          label={primaryAxis + ' distribution (justifyContent)'}
+          value={justifyContent}
+          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
+          onChange={(option) => this.setState({justifyContent: option})}
+        />
+        <Tog
+          label={secondaryAxis + ' alignment (alignItems)'}
+          value={alignItems}
+          options={['flex-start', 'center', 'flex-end', 'stretch']}
+          onChange={(option) => this.setState({alignItems: option})}
+        />
+        <View style={[styles.layout, layoutStyle]}>
+          <View style={styles.box} />
+          <View style={styles.box} />
+          <View style={styles.box} />
+        </View>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  image: {
-    width: 200,
-    height: 200,
-    position: center
+  container: {
+    flex: 1,
+  },
+  layout: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.05)',
+  },
+  box: {
+    padding: 25,
+    backgroundColor: 'steelblue',
+    margin: 5,
   },
 })

7acca9776279f132adbb107676121ae805eb5aee
Sofia <jonssonsofia93@gmail.com>
2019-11-01 14:03:06 -0700

    SectionList practice with homogenous rendering

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import { FlatList, Text, StyleSheet } from 'react-native';
+import { SectionList, Text, StyleSheet } from 'react-native';
 
-const rows = [
-  { id: 0, text: 'View' },
-  { id: 1, text: 'Text' },
-  { id: 2, text: 'Image' },
-  { id: 3, text: 'ScrollView' },
-  { id: 4, text: 'ListView' },
+const sections = [
+  {
+    id: 0,
+    title: 'Basic Components',
+    data: [
+      {id: 0, text: 'View'},
+      {id: 1, text: 'Text'},
+      {id: 2, text: 'Image'},
+    ]
+  },
+  {
+    id: 1,
+    title: 'List Components',
+    data: [
+      {id: 3, text: 'ScrollView'},
+      {id: 4, text: 'ListView'},
+    ]
+  }
 ]
 
-const extractKey = ({ id }) => id
+const extractKey = ({id}) => id
 
 export default class App extends Component {
 
-  renderItem = ({ item }) => {
+  renderItem = ({item}) => {
     return (
       <Text style={styles.row}>
         {item.text}
@@ -21,12 +33,21 @@ export default class App extends Component {
     )
   }
 
+  renderSectionHeader = ({section}) => {
+    return (
+      <Text style={styles.header}>
+        {section.title}
+      </Text>
+    )
+  }
+
   render() {
     return (
-      <FlatList
+      <SectionList
         style={styles.container}
-        data={rows}
+        sections={sections}
         renderItem={this.renderItem}
+        renderSectionHeader={this.renderSectionHeader}
         keyExtractor={extractKey}
       />
     );
@@ -41,5 +62,12 @@ const styles = StyleSheet.create({
     padding: 15,
     marginBottom: 5,
     backgroundColor: 'skyblue',
+  },
+  header: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'steelblue',
+    color: 'white',
+    fontWeight: 'bold',
   },
 })
